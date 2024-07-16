c3410b77a8b17040c1fdfd7dcec8b29e9ba3ad14
Jogchum Koerts <jogchum@clevercactus.dev>
2024-07-16 16:57:53 +0200

    refactor: make sure to skip loading if not using sqlite3

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-export default ({ /* init */ }, { database, logger }) => {
+export default (_, { database, logger }) => {
+	// Skip we are not using sqlite3;
+	if (database.client.config.client !== 'sqlite3') return
+
 	const pool = database.client.pool
 	pool.acquire().promise.then(async (conn)=>{
 		conn.loadExtension('mod_spatialite', (err) => {
